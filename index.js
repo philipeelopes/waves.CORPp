af25ee29344bac1dd4197fa7350f2d2cfab6aa69
philipe <philipedarosa10@gmail.com>
2025-08-25 23:22:18 -0300

    responsivo

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
-window,addEventListener("scroll", function() {
-    let header = this.document.querySelector('#menu')
-    header.classList.toggle('rolagem',window.scrollY > 300)
-})
 
+        
 
 
 
